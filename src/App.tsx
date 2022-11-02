ab04b80aecccf2fe04971ee048fd1a539f4e5171
Harsh <harshkrana7017@gmail.com>
2022-11-02 17:59:07 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from 'redux/store';
+
+const App: React.FC = () => (
+  <Provider store={store}>
+    <div className='flex flex-col h-full'>
+      <div className='flex flex-col items-center justify-center h-4/5'>
+        <span className='text-4xl'>Basic Setup</span>
+        <span className='text-4xl'>Vite + React + Tailwind + Redux</span>
+      </div>
+      <div className='flex flex-col items-center justify-center'>
+        <span className='text-4xl'>
+          Made with <span className='text-red-500'> &#10084;</span> by harsh
+          chlo shuru krte h
+        </span>
+      </div>
+    </div>
+  </Provider>
+);
+
+export default App;
