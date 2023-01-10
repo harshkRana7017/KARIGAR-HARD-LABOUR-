cb2fe3a9b2f414fae5a7f3f77e3ace33f5523071
Harsh <harshkrana7017@gmail.com>
2023-01-10 14:08:46 +0530

    redirects

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.scss';
 import ENV_CONFIG from 'constant/env.config';
-import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import App from './App';
 
 if (ENV_CONFIG.APP_ENV !== 'development') {
   console.log = () => {};
 }
 
 const root = createRoot(document.querySelector('#root')!);
-root.render(<BrowserRouter>
-<Provider store={store}>
-<App />
-</Provider>
-</BrowserRouter>
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
